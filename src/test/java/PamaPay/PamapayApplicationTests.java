aeb1ca12191f7027d264bfb730d1835237732cac
nikilm560 <nikilm2504@gmail.com>
2026-07-06 20:33:00 +0530

    Initial commit: set up Spring Boot project structure

---
+++ src/test/java/PamaPay/PamapayApplicationTests.java
@@ -0,0 +1,13 @@
+package PamaPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PamapayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
